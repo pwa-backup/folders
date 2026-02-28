cde7dc1e5a420f4aebb72f579b7d05c589e6b432
jlmf12 <jlmf12@gmail.com>
2026-02-28 13:28:17 +0100

    Add files via upload

---
+++ Angel-Arteaga/sw.js
@@ -0,0 +1,4 @@
+const CACHE='pwa-v1772239641665';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.json'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
